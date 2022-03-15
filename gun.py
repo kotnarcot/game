355e2232b903efc5972edf897c110a5898ddbede
arvsites <arvsites@gmail.com>
2022-03-15 17:13:16 +0500

    1

--- gun.py
+++ gun.py
@@ -8,7 +8,7 @@ class Gun():
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.botto
+        self.rect.bottom = self.screen_rect.bottom
         self.kright = False
         self.kleft = False
         self.kup = False

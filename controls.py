65e94fcee94f604a54d99d6996d36a408f4d74c1
Your Name <you@example.com>
2022-04-07 15:45:25 +0500

    my game

--- controls.py
+++ controls.py
@@ -1,29 +1,29 @@
 import sys
 import pygame
 from bullet import Bullet
-from enemy import Enemy
+# from enemy import Enemy
 
 
-def events(screen,gun, bullets ,enemy):
+def events(screen,gun, bullets ):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_d:
                 gun.kright = True
-                pygame.mixer.music.load('3.mp3')
+                pygame.mixer.music.load('1.mp3')
                 pygame.mixer.music.play(0)
             elif event.key == pygame.K_a:
                 gun.kleft = True
-                pygame.mixer.music.load('3.mp3')
+                pygame.mixer.music.load('1.mp3')
                 pygame.mixer.music.play(0)
             elif event.key == pygame.K_w:
                 gun.kup = True
-                pygame.mixer.music.load('3.mp3')
+                pygame.mixer.music.load('1.mp3')
                 pygame.mixer.music.play(-2)
             elif event.key == pygame.K_s:
                 gun.kdown = True
-                pygame.mixer.music.load('3.mp3')
+                pygame.mixer.music.load('1.mp3')
                 pygame.mixer.music.play(-2)
             elif event.key == pygame.K_SPACE:
                 new_bullet = Bullet(screen, gun)
@@ -42,8 +42,8 @@ def events(screen,gun, bullets ,enemy):
             elif event.key == pygame.K_s:
                 gun.kdown = False
 
-def update(bg_color, screen, gun, bullets , enemy):
-    screen.fill(bg_color)
+def update(bg_color, screen, gun, bullets ):
+    # screen.fill(bg_color)
     for bullet in bullets.sprites():
         bullet.drawBullet()
     gun.output()
